dbeeacf85dd5f43a76330f5c12e163214e08c296
pephistopheles <phangelov@gmail.com>
2022-10-14 11:55:00 +0300

    Spring project init

---
+++ Airbnb/src/test/java/com/ittalents/airbnb/AirbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ittalents.airbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
